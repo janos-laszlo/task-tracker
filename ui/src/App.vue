ff8907225e9ea9fd678094e20fb99d3001775d6d
janos-laszlo <janos.laszlo@electrolux.com>
2019-10-25 16:16:00 +0300

    Favicon on nav-bar

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -18,13 +18,6 @@ export default {
 <style>
 body {
   margin: 0;
-}
-
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: 'Lato', sans-serif;
 }
 </style>
--- ui/src/components/nav-bar/nav-bar.vue
+++ ui/src/components/nav-bar/nav-bar.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="topnav" id="myTopnav">
-    <a href="#home" class="active">Home</a>
-    <a href="#news">News</a>
-    <a href="#contact">Contact</a>
-    <a href="#about">About</a>
+    <a href="#home" style="padding: 5px 16px 0px 16px;">
+      <img src="@/assets/logo.png" class="logo" />
+    </a>
+    <a href="#sign-up" class="float-right active">Sign up</a>
+    <a href="#sign-in" class="float-right">Sign in</a>
     <div class="container" v-on:click="toggleMenu()">
       <div class="bar1"></div>
       <div class="bar2"></div>
