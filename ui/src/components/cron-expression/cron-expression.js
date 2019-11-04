ce471399f4c2b954564ebb4fbe6c73a7b0a8d089
Laszlo Janos <janos-laszlo@outlook.com>
2019-11-04 23:15:09 +0200

    cron-expression component - min date time

--- ui/src/components/cron-expression/cron-expression.js
+++ ui/src/components/cron-expression/cron-expression.js
@@ -1,6 +1,8 @@
 import VCalendar from "v-calendar";
 import { TimePicker } from "element-ui";
 
+let minuteUpdateIntervalId;
+
 export default {
   name: "cron-expression",
   components: {
@@ -14,8 +16,8 @@ export default {
   data() {
     return {
       frequency: "Once",
-      dateTime: new Date(),
-      minDateTime: new Date()
+      dateTime: null,
+      minDate: null
     };
   },
   computed: {
@@ -27,16 +29,26 @@ export default {
         this.frequency = value;
       }
     },
-    selectedDateTime: {
-      get() {
-        return this.dateTime;
-      },
-      set(value) {
-        this.dateTime.setHours(value.getHours());
-      }
-    },
     selectableRange: function() {
       return ["00:00", "23:59"];
+    }
+  },
+  created() {
+    this.dateTime = new Date();
+    this.dateTime.setSeconds(0);
+    if (this.future) {
+      this.minDate = new Date();
+      this.minDate.setHours(0);
+      this.minDate.setMinutes(0);
+      this.minDate.setSeconds(0);
+      minuteUpdateIntervalId = setInterval(() => {
+        this.minDate.setMinutes(this.minDate.getMinutes() + 1);
+      }, 60000);
+    }
+  },
+  destroyed() {
+    if (minuteUpdateIntervalId) {
+      clearInterval(minuteUpdateIntervalId);
     }
   }
 };
--- ui/src/components/cron-expression/cron-expression.vue
+++ ui/src/components/cron-expression/cron-expression.vue
@@ -8,12 +8,12 @@
       <option>Yearly</option>
       <option>CRON</option>
     </select>
-    <vc-date-picker v-model="selectedDateTime" :min-date="minDateTime" :first-day-of-week="2" color="blue" is-inline is-expanded />
+    <vc-date-picker v-model="dateTime" :min-date="minDate" :first-day-of-week="2" color="blue" is-inline is-expanded />
     <div class="time-picker-container">
-      <el-time-picker v-model="selectedDateTime" :selectableRange="selectableRange" format="HH:mm" placeholder="Select time"></el-time-picker>
+      <el-time-picker v-model="dateTime" :selectableRange="selectableRange" format="HH:mm" placeholder="Select time"></el-time-picker>
     </div>
     <p>frequency: {{selectedFrequency}}</p>
-    <p>date time: {{selectedDateTime}}</p>
+    <p>date time: {{dateTime}}</p>
   </div>
 </template>
 
