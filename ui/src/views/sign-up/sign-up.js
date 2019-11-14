8d8065e925b17ab3dce8faacda2870befc5413a4
janos-laszlo <janos.laszlo@electrolux.com>
2019-11-14 14:28:39 +0200

    Sign up view form validation

--- ui/src/views/sign-up/sign-up.js
+++ ui/src/views/sign-up/sign-up.js
@@ -1,8 +1,36 @@
+let onlineCheckInterval;
+
 export default {
   name: "sign-up",
   data() {
     return {
-      data: "Sign up"
+      username: '',
+      password: '',
+      confirmPassword: '',
+      submittedOnce: false,
+      online: true
     };
+  },
+  methods: {
+    onSubmit
+  },
+  created() {
+    onlineCheckInterval = setInterval(() => {
+      this.online = navigator.onLine;
+    }, 1000);
+  },
+  destroyed(){
+    clearInterval(onlineCheckInterval);
   }
 };
+
+
+function onSubmit() {
+  this.submittedOnce = true;
+  if (!formValid.call(this)) return;
+  this.$router.push('/');
+}
+
+function formValid() {
+  return this.username && this.password && this.password === this.confirmPassword;
+}
--- ui/src/views/sign-up/sign-up.vue
+++ ui/src/views/sign-up/sign-up.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="container">
-    <h2>{{data}}</h2>
+    <h2 style="text-align: center">Sign up</h2>
+    <form @submit.prevent="onSubmit">
+      <div class="input-group">
+        <input type="text" placeholder="Username" v-model="username" :readonly="!online" />
+        <span v-show="submittedOnce && !username" class="validation-error">Username required.</span>
+      </div>
+      <div class="input-group">
+        <input type="password" placeholder="Password" v-model="password" :readonly="!online" />
+        <span v-show="submittedOnce && !password" class="validation-error">Password required.</span>
+      </div>
+
+      <div class="input-group">
+        <input
+          type="password"
+          placeholder="Confirm password"
+          v-model="confirmPassword"
+          :readonly="!online"
+        />
+        <span
+          v-show="submittedOnce && confirmPassword !== password"
+          class="validation-error"
+        >Passwords don't match.</span>
+      </div>
+
+      <input type="submit" value="Sign up" class="btn btn-positive" :disabled="!online" />
+    </form>
+    <p v-show="!online" class="validation-error">Signing up requires an internet connection.</p>
   </div>
 </template>
 
